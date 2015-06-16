478aa0062a6e33c8420cf56629c967a3a2272feb
retk <ersek.attila@hotmail.com>
2015-06-16 20:41:13 +0200

    added helloWorld.ps1

---
+++ helloWorld.ps1
@@ -0,0 +1,16 @@
+﻿#init
+Add-AzureAccount
+Select-AzureSubscription msdn
+Switch-AzureMode -Name AzureResourceManager
+
+#vars
+$abk01 = "ABK01"
+
+#group
+New-AzureResourceGroup -Name $abk01 -Location "West Europe" -Force
+
+#deployment
+New-AzureResourceGroupDeployment `
+    -Name "ABK01_HELLO01" `
+    -ResourceGroupName $abk01 `
+    -TemplateFile "https://raw.githubusercontent.com/mshudx/AzureResourceManager-Examples/master/helloWorld.json"
